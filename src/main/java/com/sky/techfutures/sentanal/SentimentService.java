c8a8bd39ef67756e8923c14c163095c8b966ad93
fuzzy-logic <spam.gawain@gmail.com>
2014-03-13 17:07:21 +0000

    changed service ports

--- src/main/java/com/sky/techfutures/sentanal/SentimentService.java
+++ src/main/java/com/sky/techfutures/sentanal/SentimentService.java
@@ -6,6 +6,7 @@ import com.sky.techfutures.sentanal.resources.SentimentResource;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.config.HttpConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,15 +22,28 @@ public class SentimentService extends Service<SentimentConfiguration>   {
     @Override
     public void initialize(Bootstrap<SentimentConfiguration> bootstrap) {
         LOG.info("initialize()");
-        bootstrap.setName("asapapi");
+        bootstrap.setName("sentanal");
     }
 
     @Override
     public void run(SentimentConfiguration configuration, Environment environment) {
-        //final String serviceName = configuration.getServiceName();
-        LOG.info("run() adding TagCloudResource resource and healthcheck");
+        final String serviceName = configuration.getServiceName();
+
+        HttpConfiguration httpConfig = new HttpConfiguration();
+        httpConfig.setPort(9000);
+        httpConfig.setAdminPort(9001);
+        configuration.setHttpConfiguration(httpConfig);
+
+        int port = configuration.getHttpConfiguration().getPort();
+        int adminPort = configuration.getHttpConfiguration().getAdminPort();
+
+        LOG.info("run() serviceName: " + serviceName);
+        LOG.info("run() http port: " + port);
+        LOG.info("run() http admin port: " + adminPort);
+        LOG.info("run() adding SentimentResource and healthcheck");
         SentimentResource sentimentResource = new SentimentResource();
         environment.addResource(sentimentResource);
+
         environment.addHealthCheck(new SentimentResourceHealthCheck(sentimentResource));
     }
 
