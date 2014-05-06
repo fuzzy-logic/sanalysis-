aed52a92281addb92e157f467f12ea17bcf12ceb
fuzz-logic <spam.gawain@gmail.com>
2014-05-06 16:28:52 +0000

    made compatible for spaas

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 
 mvn clean
 mvn package
-./start.sh
+java -Xms512m -Xmx2600m -XX:PermSize=512m -jar target/sentanal-1.0-SNAPSHOT.jar server sentanal.yml
 
--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 PID=`ps -ef | grep -i "target/sentanal" | grep -v grep | awk '{print $2}'`
+echo Sentanal running PID=$IPD
 kill $PID
 
