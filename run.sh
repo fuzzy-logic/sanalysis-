3f7bc34705bfaefb1afa17932ded26f6afc64407
Gawain Hammond <spam.gawain@gmail.com>
2014-05-16 14:05:44 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-
+echo "mvn clean: cleaning project..."
 mvn clean
+echo "mvn package: building project..."
 mvn package
+echo
+echo "****************************************************************************"
+echo "running sentanal server"
+echo "****************************************************************************"
+echo
 java -Xms512m -Xmx2600m -XX:PermSize=512m -jar target/sentanal-1.0-SNAPSHOT.jar server sentanal.yml
 
